66229f10c445f29733d3142765c7fcaecccf66dc
Chaoming Li <email@chaoming.li>
2024-08-18 11:34:46 +1000

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const { default: axios } = require("axios");
+
+function AuthAlertAPI(projectId, data) {
+    // read device_id from local storage
+    var device_id = localStorage.getItem(projectId+'_device_id');
+    // generate device id if not exists
+    if (device_id === null) {
+        device_id = Math.random().toString(36).substring(2);
+        localStorage.setItem(projectId+'_device_id', device_id);
+    }
+    // add device id to data
+    data.device_id = device_id;
+    return axios.post(`https://api.authalert.com/projects/${projectId}`, data);
+}
+
+module.exports = AuthAlertAPI;
